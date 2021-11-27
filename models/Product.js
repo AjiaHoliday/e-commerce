de66e8f30612ca2813d6fabcbafa66b289cc819b
ajia <ajialeeholiday@gmail.com>
2021-11-26 20:55:01 -0700

    added all models

--- models/Product.js
+++ models/Product.js
@@ -10,6 +10,40 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [4]
+      }
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true
+      }
+    },
+    instock: {
+      type: DataTypes.INTEGER,
+      defaultValue: 5, 
+      validate: {
+        isNumeric: true
+      }
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
